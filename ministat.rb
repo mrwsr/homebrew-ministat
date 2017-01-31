cbd153ebbda37cbbaf3b2216781276026d8815db
Mark Williams <mwilliams@shoprunner.com>
2017-01-30 17:05:08 -0800

    Don't use master and don't use sha1

--- ministat.rb
+++ ministat.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Ministat < Formula
   version '0.1'
   homepage 'https://github.com/codahale/ministat'
-  url 'https://github.com/codahale/ministat/tarball/master'
-  sha1 'd2a2e4850c58ee942290475befef9d8a89c8c531'
+  url 'https://github.com/codahale/ministat/tarball/5f1a5b59077c2a7837a37454a548ecd12532b7f8'
+  sha256 'ad73993469df4a61c904cf22c7772bcfeb68bf0723aab2e162eaed48e1fbec61'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
